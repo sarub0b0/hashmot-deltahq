c65aeb5d790d956eec2e66fe976635fd9ac0d200
kosay <ekr59uv25@gmail.com>
2018-11-10 20:41:50 +0900

    Change 初期値を全部1行jsonにしてたのを，オブジェクトごとにjsonにする．mtuとかバッファの関係

--- bin/create_init_json.py
+++ bin/create_init_json.py
@@ -3,22 +3,36 @@ import random
 import json
 import sys
 
+def write_file(fd, root):
+    out = json.dumps(root, separators=(',' ':'))
+    out = out + "\n"
+    fd.write(out)
+
+
 if len(sys.argv) < 4:
     print("arg: node_number, max_number(x, y), radius\n")
+    sys.exit(1)
 
 node_num = int(sys.argv[1])
 
-root = json.loads('{}')
-root['init'] = {}
-root['init']['node'] = []
-root['init']['connection'] = []
+#  root = json.loads('{}')
+#  root['init'] = {}
+#  root['init']['node'] = []
+#  root['init']['connection'] = []
 
 max_num = int(sys.argv[2])
 radius = int(sys.argv[3])
 
+fd = open(str(node_num) + "node.json", 'w')
+
 for i in range(node_num):
-    root['init']['node'].append({
-        'name': 'node00' + str(i),
+    root = json.loads('{}')
+    root['init'] = {}
+    #  root['init']['node'] = {}
+
+    #  root['init']['node'].append({
+    root['init']['node'] = {
+        'name': 'node' + str(i),
         'type': 'regular',
         'connection': 'infrastructure',
         'adapter': 'orinoco',
@@ -28,24 +42,41 @@ for i in range(node_num):
         'Pt': 20,
         'internal_delay': 1,
         'radius': radius,
-    })
+    }
 
-    print(root["init"]["node"][-1]['x'], root["init"]["node"][-1]['y'])
+        #  print(root["init"]["node"][-1]['x'], root["init"]["node"][-1]['y'])
+    write_file(fd, root)
 
-    root['init']['connection'].append({
-        'from_node': 'node00' + str(i),
+root = json.loads('{}')
+root['init'] = {}
+
+root['init']['environment'] = {
+            'name': 'env_outdoor',
+            'is_dynamic': False,
+            'alpha': 3.2,
+            'sigma': 0.0,
+            'W': 0.0,
+            'noise_power': -100.0
+        }
+
+write_file(fd, root)
+
+for i in range(node_num):
+    root = json.loads('{}')
+    root['init'] = {}
+    #  root['init']['node'] = []
+    #  root['init']['connection'] = []
+
+    #  root['init']['connection'].append({
+    root['init']['connection'] = {
+        'from_node': 'node' + str(i),
         'to_node': 'auto_connect',
         'through_environment': 'env_outdoor',
         'standard': '802.11b',
         'packet_size': 1024,
         'consider_interference': False
-    })
+    }
+
+    write_file(fd, root)
 
 
-fd = open(str(node_num) + "node.json", 'w')
-
-out = json.dumps(root, separators=(',' ':'))
-
-out = out + "\n"
-
-fd.write(out)
--- bin/mobility.py
+++ bin/mobility.py
@@ -8,17 +8,38 @@ from signal import signal, SIGPIPE, SIG_DFL
 
 if __name__ == '__main__':
 
+    if len(sys.argv) < 4:
+        print("arguments:")
+        print("\t$1   filename")
+        print("\t$2   max loop")
+        print("\t$3   delay")
+        sys.exit(1)
+
     signal(SIGPIPE, SIG_DFL)
 
+    node = json.loads("[]")
+
+
     f = open(sys.argv[1], 'r')
-    json_init = json.load(f)
+    line = f.readline()
+    while line:
+        j = json.loads(line)
+
+        init = j['init']
+        if 'node' in init:
+            n = init['node']
+            node.append(n)
 
 
-    sys.stdout.write(json.dumps(json_init))
-    sys.stdout.write("\n")
-    sys.stdout.flush()
+        sys.stdout.write(json.dumps(j))
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+        line = f.readline()
 
     #  sys.exit()
+
+    #  print(json.dumps(node))
+    #  print("node length: %d" % len(node))
 
     time.sleep(0.5)
 
@@ -26,6 +47,10 @@ if __name__ == '__main__':
         loop_max = int(sys.argv[2])
     else:
         loop_max = 0
+
+    delay = 0.5
+    if 0 < float(sys.argv[3]):
+        delay = float(sys.argv[3])
 
     loop_count = 0
     while True:
@@ -38,13 +63,13 @@ if __name__ == '__main__':
 
                 sys.exit(1)
 
-        idx = random.randint(0, len(json_init['init']['node']) - 1)
+        idx = random.randint(0, len(node) - 1)
 
         speed = 20
 
-        x = json_init['init']['node'][idx]['x']
-        y = json_init['init']['node'][idx]['y']
-        r = json_init['init']['node'][idx]['radius']
+        x = node[idx]['x']
+        y = node[idx]['y']
+        r = node[idx]['radius']
 
         x = round(random.uniform(x - speed, y + speed), 1)
         y = round(random.uniform(y - speed, y + speed), 1)
@@ -56,13 +81,14 @@ if __name__ == '__main__':
         json_update['y'] = y
         json_update['r'] = r
 
-        json_init['init']['node'][idx]['x'] = x
-        json_init['init']['node'][idx]['y'] = y
+        node[idx]['x'] = x
+        node[idx]['y'] = y
 
         #  update = {'update': []}
         #  update['update'].append(json_update)
 
-        update = {'update': json_update}
+        update = {'update': {}}
+        update['update']['node'] = json_update
 
         #  idx = 1
         #  x = json_init['init']['node'][idx]['x']
@@ -88,7 +114,7 @@ if __name__ == '__main__':
 
         #  sys.exit()
 
-        #  time.sleep(0.1)
+        time.sleep(delay)
         #  sys.exit()
 
         #  break
