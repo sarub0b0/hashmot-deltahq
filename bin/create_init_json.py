de2488bdb1aa499e6017a82a2de84789bffd366d
kosay <ekr59uv25@gmail.com>
2018-11-22 16:37:37 +0900

    update

---
+++ bin/create_init_json.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+import random
+import json
+import sys
+
+def write_file(fd, root):
+    out = json.dumps(root, separators=(',' ':'))
+    out = out + "\n"
+    fd.write(out)
+
+
+if len(sys.argv) < 4:
+    print("arg: $1 $2 $3 $4")
+    print("\t$1: node_number")
+    print("\t$2: max_number(x, y), radius")
+    print("\t$3: radius")
+    #  print("\t$4: single or multi line")
+    sys.exit(1)
+
+node_num = int(sys.argv[1])
+
+max_num = int(sys.argv[2])
+radius = int(sys.argv[3])
+
+fd = open(str(node_num) + "node.json", 'w')
+
+
+#  if sys.argv[4] == "m":
+
+#      for i in range(node_num):
+#          root = json.loads('{}')
+#          root['init'] = {}
+#          #  root['init']['node'] = {}
+
+#          #  root['init']['node'].append({
+#          root['init']['node'] = {
+#              'name': 'node' + str(i),
+#              'type': 'regular',
+#              'connection': 'infrastructure',
+#              'adapter': 'orinoco',
+#              'x': round(random.uniform(0, max_num), 2),
+#              'y': round(random.uniform(0, max_num), 2),
+#              'z': 0,
+#              #  'z': round(random.uniform(0, max_num), 2),
+#              'Pt': 20,
+#              'internal_delay': 1,
+#              'radius': radius,
+#          }
+
+#              #  print(root["init"]["node"][-1]['x'], root["init"]["node"][-1]['y'])
+#          write_file(fd, root)
+
+#      root = json.loads('{}')
+#      root['init'] = {}
+
+#      root['init']['environment'] = {
+#                  'name': 'env_outdoor',
+#                  'is_dynamic': False,
+#                  'alpha': 3.2,
+#                  'sigma': 0.0,
+#                  'W': 0.0,
+#                  'noise_power': -100.0
+#              }
+
+#      write_file(fd, root)
+
+#      for i in range(node_num):
+#          root = json.loads('{}')
+#          root['init'] = {}
+#          #  root['init']['node'] = []
+#          #  root['init']['connection'] = []
+
+#          #  root['init']['connection'].append({
+#          root['init']['connection'] = {
+#              'from_node': 'node' + str(i),
+#              'to_node': 'auto_connect',
+#              'through_environment': 'env_outdoor',
+#              'standard': '802.11b',
+#              'packet_size': 1024,
+#              'consider_interference': False
+#          }
+
+#          write_file(fd, root)
+#  else:
+root = json.loads('{}')
+root['init'] = {}
+root['init']['node'] = []
+root['init']['environment'] = []
+root['init']['connection'] = []
+
+for i in range(node_num):
+    root['init']['node'].append({
+        'name': 'node' + str(i),
+        'type': 'regular',
+        'connection': 'infrastructure',
+        'adapter': 'orinoco',
+        'x': round(random.uniform(0, max_num), 2),
+        'y': round(random.uniform(0, max_num), 2),
+        #  'z': round(random.uniform(0, max_num), 2),
+        'z': 0,
+        'Pt': 20,
+        'internal_delay': 1,
+        'radius': radius,
+    })
+
+    root['init']['connection'].append({
+        'from_node': 'node' + str(i),
+        'to_node': 'auto_connect',
+        'through_environment': 'env_outdoor',
+        'standard': '802.11b',
+        'packet_size': 1024,
+        'consider_interference': False
+    })
+
+
+root['init']['environment'].append({
+            'name': 'env_outdoor',
+            'is_dynamic': False,
+            'alpha': 3.2,
+            'sigma': 0.0,
+            'W': 0.0,
+            'noise_power': -100.0
+        })
+write_file(fd, root)
+#  fd.write("{\"init\":\"finish\"}\n")
+#  fd.write("{\"finish\":\"finish\"}\n")
+fd.close()
---
+++ bin/mobility.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+
+import random
+import time
+import json
+import pprint
+import sys
+import numpy as np
+
+from signal import signal, SIGPIPE, SIG_DFL
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 6:
+        print("arguments:")
+        print("\t$1   filename")
+        print("\t$2   max loop")
+        print("\t$3   delay")
+        print("\t$4   node speed")
+        print("\t$5   update random or linear[r, l]")
+        #  print("\t$6   single or multi line [s, m]")
+
+        sys.exit(1)
+
+    signal(SIGPIPE, SIG_DFL)
+
+    # filename
+    f = open(sys.argv[1], 'r')
+
+    # max loop
+    if 0 < int(sys.argv[2]):
+        loop_max = int(sys.argv[2])
+    else:
+        loop_max = 0
+
+    # delay
+    delay = 0
+    if 0 < float(sys.argv[3]):
+        delay = float(sys.argv[3])
+
+    # node speed
+    speed = int(sys.argv[4])
+
+    # update random or linear
+    is_random = False
+    if sys.argv[5] == "r":
+        is_random = True
+    else:
+        is_random = False
+
+    # single or multi line
+    #  is_single = False
+    #  if sys.argv[6] == "s":
+    #      is_single = True
+    #  else:
+    #      is_single = False
+
+
+    #  if is_single:
+    line = f.readline()
+    j = json.loads(line)
+        #  sys.stdout.write(json.dumps(j, separators=(',' ':')))
+        #  sys.stdout.write("\n")
+        #  sys.stdout.flush()
+
+    node = j["init"]["node"]
+
+        #  line = f.readline()
+        #  j = json.loads(line)
+        #  sys.stdout.write(json.dumps(j, separators=(',' ':')))
+        #  sys.stdout.write("\n")
+        #  sys.stdout.flush()
+
+
+    #  else:
+    #      node = json.loads("[]")
+    #      line = f.readline()
+    #      while line:
+    #          j = json.loads(line)
+
+    #          init = j['init']
+    #          if 'node' in init:
+    #              n = init['node']
+    #              node.append(n)
+
+
+    #          #  sys.stdout.write(json.dumps(j, separators=(',' ':')))
+    #          #  sys.stdout.write("\n")
+    #          #  sys.stdout.flush()
+    #          line = f.readline()
+
+    node = np.array(node)
+
+    #  time.sleep(0.2)
+    #  sys.exit()
+
+    #  print(json.dumps(node))
+    #  print("node length: %d" % len(node))
+
+
+
+    mt = 0
+
+    loop_count = 0
+
+    idx = 0
+    while True:
+        if 0 < loop_max:
+            if loop_max <= loop_count:
+                sys.stdout.write('{"finish":"finish"}')
+                sys.stdout.write("\n")
+                sys.stdout.flush()
+                #  time.sleep(2)
+
+                sys.exit(1)
+
+        t0 = time.perf_counter()
+
+        if is_random:
+            idx = random.randint(0, len(node) - 1)
+        else:
+            idx = idx % len(node)
+
+        x = node[idx]['x']
+        y = node[idx]['y']
+        r = node[idx]['radius']
+
+        x = round(random.uniform(x - speed, y + speed), 1)
+        y = round(random.uniform(y - speed, y + speed), 1)
+
+
+        json_update = {}
+        json_update['id'] = idx
+        json_update['x'] = x
+        json_update['y'] = y
+        json_update['r'] = r
+
+        node[idx]['x'] = x
+        node[idx]['y'] = y
+
+        #  update = {'update': []}
+        #  update['update'].append(json_update)
+
+        update = {'update': {}}
+        update['update']['node'] = json_update
+
+        #  idx = 1
+        #  x = json_init['init']['node'][idx]['x']
+        #  y = json_init['init']['node'][idx]['y']
+
+        #  speed = 10
+        #  json_update = {}
+        #  json_update['id'] = idx
+        #  json_update['x'] = round(random.uniform(x - speed, y + speed), 1)
+        #  json_update['y'] = round(random.uniform(y - speed, y + speed), 1)
+
+        #  update['update'].append(json_update)
+
+
+
+        #  print(json.dumps(update))
+
+        sys.stdout.write(json.dumps(update, separators=(',' ':')))
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+
+        loop_count += 1
+
+        #  sys.exit()
+
+        t1 = time.perf_counter()
+
+        mt = mt + (t1 - t0)
+
+        #  print("%04d elapsed: %f" % (idx, (t1 - t0)))
+
+        idx = idx + 1
+
+        time.sleep(delay)
+        #  sys.exit()
+
+        #  break
+
+    print("all elapsed: %f" % mt)
