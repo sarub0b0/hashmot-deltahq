cd14d69d2b7ef2f4f73bbd8221fb415b97cf872f
kosay <ekr59uv25@gmail.com>
2018-11-23 17:31:26 +0900

    create radius threashold caluculator

---
+++ bin/threshold_calculation.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import random
+import time
+import json
+import pprint
+import sys
+import numpy as np
+
+from signal import signal, SIGPIPE, SIG_DFL
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print("arguments:")
+        print("\t$1   filename")
+        print("\t$2   delay")
+        #  print("\t$6   single or multi line [s, m]")
+
+        sys.exit(1)
+
+    signal(SIGPIPE, SIG_DFL)
+
+    # filename
+    f = open(sys.argv[1], 'r')
+
+    # delay
+    delay = 0
+    if 0 < float(sys.argv[2]):
+        delay = float(sys.argv[2])
+
+    line = f.readline()
+    j = json.loads(line)
+
+    node = j["init"]["node"]
+
+    node = np.array(node)
+
+    loop_count = 0
+
+    idx = 0
+    while True:
+
+        idx = 0
+
+        x = node[idx]['x']
+        y = node[idx]['y']
+        r = node[idx]['radius']
+
+        if 300 < x:
+            sys.exit(1)
+            x = 1
+
+        x = x + 1
+
+        json_update = {}
+        json_update['id'] = idx
+        json_update['x'] = x
+        json_update['y'] = y
+        json_update['r'] = r
+
+        node[idx]['x'] = x
+        node[idx]['y'] = y
+
+
+        update = {'update': {}}
+        update['update']['node'] = json_update
+
+        sys.stdout.write(json.dumps(update, separators=(',' ':')))
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+
+        loop_count += 1
+
+        time.sleep(delay)
+
