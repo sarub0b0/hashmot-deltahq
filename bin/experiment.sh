f5e8ab1b888f0c4fb6f155f91103a6705fe30e88
kosay <ekr59uv25@gmail.com>
2018-12-12 20:41:22 +0900

    update

--- bin/experiment.sh
+++ bin/experiment.sh
@@ -2,27 +2,35 @@
 
 nodes=(100 200 500 1000 2000 5000 10000 20000 50000 100000)
 
+if [ ! -d ../density_json ]; then
+    for n in ${nodes[@]};
+    do
+        ./create_measured_init_json.sh $n 160
+
+    done
+fi
+
+if [ -z "$1" ]; then
+    echo "set algorithm"
+    exit
+fi
+
+algorithm=$1
+
 for n in ${nodes[@]};
 do
-    ./create_measured_init_json.sh $n 160
-
-done
-
-
-for n in ${nodes[@]};
-do
-    ./density_measure.sh $n t
+    ./density_measure.sh $n $algorithm
     cp -r ../density_json/ ../tree_density
 done
 
 for n in ${nodes[@]};
 do
-    ./density_measure.sh $n h
+    ./density_measure.sh $n $algorithm
     cp -r ../density_json/ ../hash_density
 done
 
 for n in ${nodes[@]};
 do
-    ./density_measure.sh $n l
+    ./density_measure.sh $n $algorithm
     cp -r ../density_json/ ../linear_density
 done
